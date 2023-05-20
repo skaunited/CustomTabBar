4a80849359470346c9039904fe9f176b0101721a
Timothé Micheneau <t.micheneau@workflow-dev.fr>
2023-05-20 16:03:51 +0900

    Initial Commit

---
+++ Curved Custom TabBar/Curved_Custom_TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curved_Custom_TabBarApp.swift
+//  Curved Custom TabBar
+//
+//  Created by Work Flow Dev on 20/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Curved_Custom_TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
